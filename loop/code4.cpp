7e93174fff6c430f6c5f16f6dc17cbd3c54f0139
cn-gourav <mrgusain558@gmail.com>
2025-11-27 22:57:30 +0530

    Add :: update file

--- loop/code4.cpp
+++ loop/code4.cpp
@@ -1,8 +1,25 @@
+// prime nummber 
+
 #include <iostream>
 using namespace std;
 int main() {
 
-    // Your code here 
-    
+    int num ;
+    cin>>num;
+
+    for(int i=2; i<num; i++){
+        int curr = i;
+        bool isPrime = true;
+
+       for(int j=2;j*j<=i;j++){
+        if(curr%j==0){
+            isPrime=false;
+        }
+    }    
+        if(isPrime){
+            cout<<curr<<"";
+        }
+    }
+        cout<<endl;
     return 0;
 }
