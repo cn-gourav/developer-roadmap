2145f90e3a7ff3c93d530c78d42cfbe53ae12db9
cn-gourav <mrgusain558@gmail.com>
2025-11-29 01:19:29 +0530

    update :: partice file added pattern concpet

---
+++ Partice_Qs/Pattern_Concept/code.cpp
@@ -0,0 +1,18 @@
+// Square star pattern 
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int star;
+    cin>>star;
+
+    for(int i=1; i<=star; i++){
+        for(int j=1; j<=star; j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    
+    return 0;
+}
---
+++ Partice_Qs/Pattern_Concept/code9.cpp
@@ -0,0 +1,20 @@
+// inverted & rotated half-pyramid 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n-i; j++) {
+            cout<<" ";
+        }
+        for(int m =1; m <=i; m++){
+            cout<<"*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
