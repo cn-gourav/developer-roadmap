dd6ff8fa092d85d964155df094aa9b3a0d9decee
cn-gourav <mrgusain558@gmail.com>
2025-12-16 20:46:47 +0530

    Add :: code.cpp

---
+++ Live_Partice_Session/code.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // int a = 10;
+    /* =  assign operator
+    == logical operator two value comparsion
+    */
+    // programming keyword we can used to this keyword
+
+    /*
+    write a variable
+
+    pascal case
+    usernumber
+
+    camal case
+    userNumber
+
+    snake case
+    user_name_for _student
+
+    */
+
+    // variable decleration
+    // int a, b, c;
+
+    // cin >> a >> b >> c;
+
+    // cout << a;
+    // cout << b;
+    // cout << c;
+
+    // operator
+    // int num1 = 10;
+    // int num2 = 20;
+
+    // cout << num1 - num2 << endl;
+    // cout << num1 + num2 << endl;
+    // cout << num1 * num2 << endl;
+    // cout << num1 / num2 << endl;
+    // cout << num1 % num2 << endl;
+
+    // if else
+    // ture or false
+
+    // if(true){
+    //     adding
+    // }else{
+    //     conditon not match
+    // }
+
+    // if else ladder
+
+    // if(true)
+    // else if(true)
+    // else if(true)
+    // else(print something)
+
+    // top to bottom  flow of code
+
+    // terinary operator use
+    int a = 10;
+
+    // = assign operator
+    // == comparsion operator
+    // (a == 10) ? cout<<"true" : cout<<"false";
+
+    // (a >= 0) ? cout << "its a positive number" : cout << "its not a positive number";
+
+    // (a >= 0) ? cout << a * 3 : cout << a * 5;
+
+    // swtich case
+    /*
+        switch(num):
+            case1:
+            break;
+
+            case2:
+            break;
+
+            case3:
+            break;
+
+            case4:
+            break;
+
+            default  */
+
+    // loop concept
+
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+    // cout << "Gourav gusain";
+
+    // loop repeation code program kaam kr deta
+
+    // positive loop
+    // starting value   st = 1
+    // conditiona       st < 10
+    // iterator         st ++  //++ plus operator
+
+    // negative loop
+    // starting value   st = 10
+    // conditiona       st > 0
+    // iterator         st -- // -- negative operator
+
+    // --  -1
+    // ++  +1
+
+    // a++ post increment    ++a   pre increment
+    // a-- post decrement    --a   pre decrement
+
+    // a /= 2 iska mtlb ky hoga
+    // a = a / 2 ;
+
+    // a *= 2;
+    // a = a*2;
+
+    // while loop
+    // int st = 1;
+
+    // while (st < 11){
+    //     cout << st;
+    //     st = st + 1;
+    // }
+
+    // int st = 1;
+
+    // while (true)
+    // {
+    //     if (st < 10)
+    //     {
+            // cout << st;
+            // continue;  continous 
+            // break loop if come out 
+        // }
+    // }
+
+   
+
+    return 0;
+}
