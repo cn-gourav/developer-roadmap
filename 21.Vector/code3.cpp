3e3b9ae28c55e1ff5ba7ea314ba0336163953cb1
cn-gourav <mrgusain558@gmail.com>
2025-12-29 22:47:07 +0530

    Add :: ADDED FILE

---
+++ 21.Vector/code3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int triplet(vector<int> &nums)
+{
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = 1; nums.size() - 1; i++)
+        {
+            ans =
+        }
+    }
+}
+
+int main()
+{
+
+    vector<int> nums = {-1, 0, 1, 2, -1, -4} triplet() return 0;
+}
